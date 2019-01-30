7a7eb251b44ea86c45292d14b5227a8f222b7d95
leeks <lilim2019@u.northwestern.edu>
2019-01-30 12:39:42 -0600

    untested commit

--- student_code.py
+++ student_code.py
@@ -128,7 +128,55 @@ class KnowledgeBase(object):
         printv("Retracting {!r}", 0, verbose, [fact_or_rule])
         ####################################################
         # Student code goes here
-        
+        if isinstance(fact_or_rule, Fact):  # first see if it is a rule or fact
+            fact_in_kb = None # get fact object in kb that has complete attributes
+            for f in self.facts:
+                if f == fact_or_rule:
+                    fact_in_kb = f
+            if not fact_in_kb:  # fact was not found in kb list
+                print("fact was not found in kb")  # should not happen, but in case fact not in kb
+            elif fact_in_kb.supported_by:  # if retracting assertion but still supported
+                fact_in_kb.asserted = False
+            else:  # retracting a fact that is not supported by assertion or inference
+                for supported_fact in fact_or_rule.supports_facts:  # for every fact it supports
+                    for pair in supported_fact.supported_by:  # find all pairs that contain this retracting fact
+                        if fact_or_rule in pair:  # retracting fact may support the same fact in multiple ways
+                            supported_fact.supported_by.remove(pair)  # all fact-rule pairs that must be rescinded
+                    if (not supported_fact.supported_by) and (not supported_fact.asserted):  # if no longer supported or asserted
+                        self.kb_retract(supported_fact)  # recurse onto this fact; if fact still supported, step is done
+                for supported_rule in fact_or_rule.supports_rules:  # for every rule it supports
+                    for pair in supported_rule.supported_by:
+                        if fact_or_rule in pair:
+                            supported_rule.supported_by.remove(pair)
+                    if (not supported_rule.supported_by) and (not supported_rule.asserted):  # same as above
+                        self.kb_retract(supported_rule)
+                self.facts.remove(fact_in_kb)
+        elif isinstance(fact_or_rule, Rule):
+            rule_in_kb = None
+            for r in self.rules:
+                    if r == fact_or_rule:
+                        rule_in_kb = r
+            if not rule_in_kb:
+                print("rule was not found in kb")  # same as above
+            elif rule_in_kb.supported_by:  # rule is still supported, so just remove assertion
+                rule_in_kb.asserted = False
+            else:  # assuming supported by and support directly reference each other in kb as above
+                for sf in fact_or_rule.supports_facts:  # sf is supported_fact as above
+                    for pair in sf.supported_by:
+                        if fact_or_rule in pair:
+                            sf.supported_by.remove(pair)
+                    if (not sf.supported_by) and (not sf.asserted):
+                        self.kb_retract(sf)  # no longer has any supports, so for sure retractable
+                for sr in fact_or_rule.supports_rules:
+                    for pair in sr.supported_by:
+                        if fact_or_rule in pair:
+                            sr.supported_by.remove(pair)
+                    if (not sr.supported_by) and (not sr.asserted):
+                        self.kb_retract(sr)
+                self.rules.remove(rule_in_kb)
+        else:
+            print("unsupported, not fact or rule")
+
 
 class InferenceEngine(object):
     def fc_infer(self, fact, rule, kb):
@@ -146,12 +194,24 @@ class InferenceEngine(object):
             [fact.statement, rule.lhs, rule.rhs])
         ####################################################
         # Student code goes here
-        list_of_statements = rule.lhs
-        binding = match(fact.statement, list_of_statements[0])  # Check if fact statement matches LHS statement in rule
+        left_most = rule.lhs[0]  # get left most statement in lhs of rule
+        binding = match(fact.statement, left_most)  # Check if fact statement matches LHS statement in rule
         if binding:  # if there is a match, check to see if there are more statements in the lhs
-            if len(list_of_statements) > 1:
-                new_rule = Rule([], ())  # create new inferred rule supported by fact + rule pair
-                kb.kb_assert(new_rule)  # assert into the kb
-            else:
-                new_fact = Fact(rule.rhs, ())  # create new inferred fact supported by fact + rule pair
-                kb.kb_assert(new_fact)  # assert into the kb
+            pair = (fact, rule)  # generate fact rule pair, all supported by is a fact rule pair
+            if len(rule.lhs) > 1:
+                new_lhs = []  # generate empty lists
+                new_rhs = []
+                for ls in rule.lhs[1:]:  # for every left statement in lhs
+                    new_lhs.append(instantiate(ls, binding))  # add new inferred rule's statements to lhs
+                for rs in rule.rhs:  # for every right statement in rhs
+                    new_rhs.append(instantiate(rs, binding))  # same for rhs
+                new_rule = Rule([new_lhs, new_rhs], [pair])
+                kb.kb_assert(new_rule)
+                fact.supports_rules.append(new_rule)  # add to matching rule and fact's supports rules
+                rule.supports_rules.append(new_rule)
+            else:  # assert new facts
+                for rs in rule.rhs:  # for every implied statement in the rhs
+                    new_fact = Fact(instantiate(rs, binding), [pair])
+                    kb.kb_assert(new_fact)
+                    fact.supports_facts.append(new_fact)
+                    rule.supports_facts.append(new_fact)
